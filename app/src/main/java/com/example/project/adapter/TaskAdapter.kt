6c1834aeea282ac1dc0ef204e3e86849010b53ac
hook171 <dobuls@bk.ru>
2023-01-31 07:51:48 +1000

    создаем адаптер, который будет отрисовывать наши записи. наследуем его от recyclerview adapter, указываем viewholder, он хранит в себе ссылки на наши view. binding хранит в себе всю разметку нашего task. oncreateviewholder надувает нашу разметку taska. onbindviewholder заполняет нашу разметку данными

---
+++ app/src/main/java/com/example/project/adapter/TaskAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.project.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project.databinding.TaskLayoutAdapterBinding
+import com.example.project.model.Task
+
+class TaskAdapter: RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    private var binding: TaskLayoutAdapterBinding? = null
+
+
+    class TaskViewHolder(itemBinding: TaskLayoutAdapterBinding): RecyclerView.ViewHolder(itemBinding.root)
+
+
+    private val differCallback =
+        object : DiffUtil.ItemCallback<Task>(){
+            override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Task, newItem: яTask): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+        }
+
+
+    val differ = AsyncListDiffer(this,differCallback)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+
+        val view = LayoutInflater.from(parent.context)
+
+        binding = TaskLayoutAdapterBinding.inflate(
+            view,
+            parent,
+            false
+        )
+
+        return TaskViewHolder(binding!!)
+
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val currentTask = differ.currentList[position]
+
+        holder.itemView.apply{
+            binding?.tvTaskTitle?.text = currentTask.nateTitle
+            binding?.tvTaskBody?.text = currentTask.nateBody
+        }
+
+
+    }
+
+
+}
