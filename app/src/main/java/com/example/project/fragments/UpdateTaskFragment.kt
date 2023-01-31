9d3e816b4176566fe2cb6b84e224b4d8f733275d
hook171 <dobuls@bk.ru>
2023-01-31 12:08:14 +1000

    сделал settext на запись которую мы выбрали, а также реализовал update записи через onclicklistener. если заголовок записи существует, то через viewmodel изменяем эту запись и нас выкидывает на главный экран

--- app/src/main/java/com/example/project/fragments/UpdateTaskFragment.kt
+++ app/src/main/java/com/example/project/fragments/UpdateTaskFragment.kt
@@ -1,60 +1,91 @@
 package com.example.project.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.project.MainActivity
 import com.example.project.R
+import com.example.project.adapter.TaskAdapter
+import com.example.project.databinding.FragmentUpdateTaskBinding
+import com.example.project.model.Task
+import com.example.project.toast
+import com.example.project.viewmodel.TaskViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UpdateTaskFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UpdateTaskFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentUpdateTaskBinding? = null
+    private val binding get() = _binding!!
+
+
+    private val args: UpdateTaskFragmentArgs by navArgs()
+    private lateinit var currentTask: Task
+    private lateinit var taskViewModel: TaskViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        setHasOptionsMenu(true)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_task, container, false)
+
+        _binding = FragmentUpdateTaskBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UpdateTaskFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UpdateTaskFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        taskViewModel = (activity as MainActivity).taskViewModel
+
+        currentTask = args.task!!
+
+        binding.edTaskBodyUpdate.setText(currentTask.nateBody)
+        binding.edTaskTitleUpdate.setText(currentTask.nateTitle)
+
+        binding.fabUpdate.setOnClickListener{
+
+            val title = binding.edTaskTitleUpdate.text.toString().trim()
+            val body = binding.edTaskBodyUpdate.text.toString().trim()
+
+            if(title.isNotEmpty()){
+                val task = Task(currentTask.id, title, body)
+                taskViewModel.updateTask(task)
+
+                view.findNavController().navigate(
+                    R.id.action_updateTaskFragment_to_homeFragment
+                )
             }
+            else{
+                activity?.toast("Пожалуйста введите название записи...")
+            }
+        }
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.update_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
 }
